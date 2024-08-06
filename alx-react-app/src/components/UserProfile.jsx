29647c084002087a03dc941b7a94c45a77af7f5f
Tainicknack0505 <taiwo.bandipo00@gmail.com>
2024-08-06 23:31:12 +0100

    Update UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,20 @@
-function UserProfile({ name, age, bio }) {
+// function UserProfile({ name, age, bio }) {
+//   return (
+//     <div>
+//       <h2>Name: {name}</h2>
+//       <p>Age: {age}</p>
+//       <p>Bio: {bio}</p>
+//     </div>
+//   );
+// }
+
+const UserProfile = (props) => {
   return (
     <div>
-      <h2>Name: {name}</h2>
-      <p>Age: {age}</p>
-      <p>Bio: {bio}</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
-}
-
+};
 export default UserProfile;
