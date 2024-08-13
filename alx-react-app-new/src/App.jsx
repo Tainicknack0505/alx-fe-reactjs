c7780f50df981b4261284539b2e44ba74786b0cb
Tainicknack0505 <taiwo.bandipo00@gmail.com>
2024-08-13 02:43:57 +0100

    Add my-company directory

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,9 +5,11 @@ import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
 import Counter from "./components/Counter";
+import UserContext from "./components/UserContext";
 
 function App() {
   // const [count, setCount] = useState(0);
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <>
@@ -15,6 +17,7 @@ function App() {
       <Counter />
       <Header />
       <MainContent />
+      <UserContext.Provider value={userData}></UserContext.Provider>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,18 +1,4 @@
-// import UserContext from '.components/UserContext';
-//   const useContext
-//   return (
-//     <div>
-//       <h2>{props.name}</h2>
-//       <p>Age: {props.age}</p>
-//       <p>Bio: {props.bio}</p>
-//     </div>
-//   );
-// };
-// export default UserProfile;
 
-// import React, { useContext } from 'react';
-
-// import React, { useContext } from "react";
 import UserContext from "./UserContext";
 import { useContext } from "react";
 
