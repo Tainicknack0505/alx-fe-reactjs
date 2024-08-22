8d0529d40b1fff15472cea39fd1130e76580afa0
Tainicknack0505 <taiwo.bandipo00@gmail.com>
2024-08-22 14:06:50 +0100

    Updated recipe-sharing-app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,7 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 
 const App = () => {
   return (
@@ -7,6 +9,10 @@ const App = () => {
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
       <RecipeList />
+      <RecipeList />
+      <Switch>
+        <Route path="/recipe/:id" component={RecipeDetails} />
+      </Switch>
     </div>
   );
 };
