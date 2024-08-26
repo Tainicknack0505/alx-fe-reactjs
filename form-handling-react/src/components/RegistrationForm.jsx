95e22d6e455a6a6bb0708be70f90ae176f7803a4
Tainicknack0505 <taiwo.bandipo00@gmail.com>
2024-08-26 11:01:01 +0100

    Updated form-handling-react directory

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -5,13 +5,21 @@ const RegistrationForm = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (username && email && password) {
-      console.log({ username, email, password });
+    let formErrors = {};
+
+    if (!username) formErrors.username = 'Username is required';
+    if (!email) formErrors.email = 'Email is required';
+    if (!password) formErrors.password = 'Password is required';
+
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
     } else {
-      alert('All fields are required!');
+      console.log({ username, email, password });
+      setErrors({});
     }
   };
 
@@ -24,6 +32,7 @@ const RegistrationForm = () => {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
+        {errors.username && <div style={{ color: 'red' }}>{errors.username}</div>}
       </div>
       <div>
         <label>Email:</label>
@@ -32,6 +41,7 @@ const RegistrationForm = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        {errors.email && <div style={{ color: 'red' }}>{errors.email}</div>}
       </div>
       <div>
         <label>Password:</label>
@@ -40,6 +50,7 @@ const RegistrationForm = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        {errors.password && <div style={{ color: 'red' }}>{errors.password}</div>}
       </div>
       <button type="submit">Register</button>
     </form>
