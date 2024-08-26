755875d2b5a4117d96777e7fe0bf2670b20fa55a
Tainicknack0505 <taiwo.bandipo00@gmail.com>
2024-08-26 18:45:47 +0100

    Updated react-query-demo directory

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 
 const fetchPosts = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
@@ -9,7 +10,16 @@ const fetchPosts = async () => {
 };
 
 function PostsComponent() {
-  const { data, error, isLoading, isError, refetch } = useQuery("posts", fetchPosts);
+  const { data, error, isLoading, isError, refetch } = useQuery(
+    "posts",
+    fetchPosts,
+    {
+      cacheTime: 1000 * 60 * 5, // 5 minutes
+      staleTime: 1000 * 60 * 1, // 1 minute
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
+    }
+  );
 
   if (isLoading) {
     return <div>Loading...</div>;
