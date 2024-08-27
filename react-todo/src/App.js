2f2aa12e2a13c6b0923e9866143f12020dd01a2b
Tainicknack0505 <taiwo.bandipo00@gmail.com>
2024-08-27 04:49:29 +0100

    Add react-todo directory

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -23,3 +23,16 @@
 // }
 
 // export default App;
+
+import React from 'react';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
