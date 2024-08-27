2a16a767680b41ca54a6720dffb0ca1d5354072a
Tainicknack0505 <taiwo.bandipo00@gmail.com>
2024-08-27 04:45:12 +0100

    Add react-todo directory

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,35 @@
+// src/__tests__/TodoList.test.js
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import TodoList from '../TodoList';
+
+test('renders TodoList component', () => {
+  render(<TodoList />);
+  expect(screen.getByText('Todo List')).toBeInTheDocument();
+});
+
+test('renders initial todos', () => {
+  render(<TodoList />);
+  expect(screen.getByText('Learn React')).toBeInTheDocument();
+  expect(screen.getByText('Build a Todo App')).toBeInTheDocument();
+});
+
+test('adds a new todo', () => {
+  render(<TodoList />);
+  fireEvent.change(screen.getByPlaceholderText('Add a new todo'), { target: { value: 'New Todo' } });
+  fireEvent.click(screen.getByText('Add'));
+  expect(screen.getByText('New Todo')).toBeInTheDocument();
+});
+
+test('toggles a todo', () => {
+  render(<TodoList />);
+  fireEvent.click(screen.getByText('Learn React'));
+  expect(screen.getByText('Learn React')).toHaveStyle('text-decoration: line-through');
+});
+
+test('deletes a todo', () => {
+  render(<TodoList />);
+  fireEvent.click(screen.getByText('Delete'));
+  expect(screen.queryByText('Learn React')).not.toBeInTheDocument();
+});
