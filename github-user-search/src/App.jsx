c19950ab970a2e360b0940c4100615ebfdbfc6c2
Tainicknack0505 <taiwo.bandipo00@gmail.com>
2024-09-18 08:33:27 +0100

    Add github-user-search directory

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,20 +1,35 @@
-import React from "react";
-import SearchBar from "./components/SearchBar";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-gray-200 p-8 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold mb-4 text-white text-center">
-          GitHub User Search Application
-        </h1>
-        <p className="text-white text-center">
-          Search for GitHub profiles by username
-        </p>
-        <SearchBar />
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
