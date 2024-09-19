2978b07db8d6e63992162ff7e654c004df09d888
Tainicknack0505 <taiwo.bandipo00@gmail.com>
2024-09-19 17:32:45 +0100

    Update github-user-search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,26 +1,26 @@
 
 
-// export const searchUsers = async ({ username, location, minRepos, page }) => {
-//     const query = [];
-//     if (username) query.push(`user:${username}`);
-//     if (location) query.push(`location:${location}`);
-//     if (minRepos) query.push(`repos:>${minRepos}`);
+export const searchUsers = async ({ username, location, minRepos, page }) => {
+    const query = [];
+    if (username) query.push(`user:${username}`);
+    if (location) query.push(`location:${location}`);
+    if (minRepos) query.push(`repos:>${minRepos}`);
   
-//     const queryString = query.join('+');
-//     const url = `https://api.github.com/search/users?q=${queryString}&page=${page}`;
+    const queryString = query.join('+');
+    const url = `https://api.github.com/search/users?q=${queryString}&page=${page}`;
   
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
   
-//     const data = await response.json();
-//     return data.items;
-//   };
+    const data = await response.json();
+    return data.items;
+  };
 
-import axios from "axios";
+// import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
-};
+// export const fetchUserData = async (username) => {
+//   const response = await axios.get(`https://api.github.com/users/${username}`);
+//   return response.data;
+// };
