128d5b08a1444aee07d9cbcf0f58081c29faa4a4
Tainicknack0505 <taiwo.bandipo00@gmail.com>
2024-09-21 09:37:57 +0100

    Update github-user-search directory

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react'
+import Search from './components/Search'
 
-function App() {
-  return <></>;
+const App = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  )
 }
 
-export default App;
+export default App
